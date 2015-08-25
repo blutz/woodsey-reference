8d1f11d0212e659fa10a80684b3de861c3e8ecde
Byron Lutz <byronlutz@gmail.com>
2015-08-25 12:22:28 -0700

    Set up basic view with angular and foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require vendor/angular
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
